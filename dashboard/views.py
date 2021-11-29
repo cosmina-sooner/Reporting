f25bef2c422b0f8f75d0488e3532d602352cd5f1
cosminasolomon <solomoncosminamaria@gmail.com>
2021-11-29 17:02:28 +0100

    add selector by year/period/country

--- dashboard/views.py
+++ dashboard/views.py
@@ -5,3 +5,31 @@ from django.shortcuts import render
 # View for index page. 
 def homepage(request):
   return render(request, 'dashboard/homepage.html')
+
+
+
+# Import mimetypes module
+import mimetypes
+# import os module
+import os
+# Import HttpResponse module
+from django.http.response import HttpResponse
+
+
+def download_file(request):
+    # Define Django project base directory
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    # Define text file name
+    filename = 'test.txt'
+    # Define the full file path
+    filepath = BASE_DIR + '/dashboard/templates/dashboard/' + filename
+    # Open the file for reading content
+    path = open(filepath, 'r')
+    # Set the mime type
+    mime_type, _ = mimetypes.guess_type(filepath)
+    # Set the return value of the HttpResponse
+    response = HttpResponse(path, content_type=mime_type)
+    # Set the HTTP header for sending to browser
+    response['Content-Disposition'] = "attachment; filename=%s" % filename
+    # Return the response value
+    return response
